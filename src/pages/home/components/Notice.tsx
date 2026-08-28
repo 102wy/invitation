b9050609bc5566e41e2de678c3edbeaa11a3d549
102 <89123240+102wy@users.noreply.github.com>
2026-08-28 15:51:27 +0900

    map icon 변경, 프레임 이미지 예시 추가

--- src/pages/home/components/Notice.tsx
+++ src/pages/home/components/Notice.tsx
@@ -1,6 +1,9 @@
 import MainTitle from "../../../components/ui/MainTitle";
 import * as S from "./Notice.style";
 
+import FRAME1 from "@/assets/images/frame_1.jpg";
+import FRAME2 from "@/assets/images/frame_2.jpg";
+
 const Notice = () => {
   return (
     <S.Wrap>
@@ -9,11 +12,11 @@ const Notice = () => {
 
       <div className="img_wrap">
         <div className="content">
-          <img src="" alt="" />
+          <img src={FRAME1} alt="" />
           <p>신랑·신부 프레임</p>
         </div>
         <div className="content">
-          <img src="" alt="" />
+          <img src={FRAME2} alt="" />
           <p>후추·먼지 프레임</p>
         </div>
       </div>
@@ -21,7 +24,7 @@ const Notice = () => {
         로비에 포토부스가 마련되어 있습니다.
         <br />두 가지 프레임이 준비되어 있으니
         <br />
-        모두 이용해 보시고 소중한 추억을 남겨 주세요. ♥
+        모두 이용해 보시고 소중한 추억을 남겨 주세요<span className="heart">♥</span>
       </p>
     </S.Wrap>
   );
